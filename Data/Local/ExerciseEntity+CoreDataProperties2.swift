6d719e048bd1cedb30320aed7df96d209df3324c
isk0nka-dot <isk0nka@bk.ru>
2026-02-12 13:59:09 +0500

    Create ExerciseEntity+CoreDataProperties2.swift

---
+++ Data/Local/ExerciseEntity+CoreDataProperties2.swift
@@ -0,0 +1,15 @@
+import Foundation
+import CoreData
+
+extension ExerciseEntity {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<ExerciseEntity> {
+        return NSFetchRequest<ExerciseEntity>(entityName: "ExerciseEntity")
+    }
+
+    @NSManaged public var id: String?
+    @NSManaged public var name: String?
+    @NSManaged public var desc: String?
+    @NSManaged public var category: String?
+    @NSManaged public var imageUrl: String?
+}
